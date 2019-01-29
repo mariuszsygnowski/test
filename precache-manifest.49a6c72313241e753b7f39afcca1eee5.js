60c0403d7927c7ad454c8bc82c780fc134af0967
Mariusz Sygnowski <sygnowski.m@gmail.com>
2019-01-29 12:03:05 +0000

    first commit

---
+++ precache-manifest.49a6c72313241e753b7f39afcca1eee5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "256ebe4887ef3ac2dca7827ddf25bbac",
+    "url": "/test/static/media/logo.256ebe48.svg"
+  },
+  {
+    "revision": "c405c13ebacaadfc5def2aa6f9ae7b7d",
+    "url": "/test/static/media/icons_mobiscroll.c405c13e.ttf"
+  },
+  {
+    "revision": "965a7a1a7bd29d1565b5edeca7b4b9b7",
+    "url": "/test/static/media/icons_mobiscroll.965a7a1a.woff"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "3118269a7c6a5dce38e0",
+    "url": "/test/static/js/main.3118269a.chunk.js"
+  },
+  {
+    "revision": "ea7f5e5122d3f5769de3",
+    "url": "/test/static/js/1.ea7f5e51.chunk.js"
+  },
+  {
+    "revision": "3118269a7c6a5dce38e0",
+    "url": "/test/static/css/main.75b4cd34.chunk.css"
+  },
+  {
+    "revision": "ea7f5e5122d3f5769de3",
+    "url": "/test/static/css/1.b826f8a7.chunk.css"
+  },
+  {
+    "revision": "c2b2c0abbd1909948b359a3ac060b1e8",
+    "url": "/test/index.html"
+  }
+];
